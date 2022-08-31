289844d79f30c773455eb2f317b75c9047c47755
WHOMEL <williamhomel@gmail.com>
2022-08-31 12:15:44 -0700

    Contact Page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
+        <Route path='about' element={<AboutPage/>}/>
         <Route path='directory' element={<CampsitesDirectoryPage />}/>
         <Route
             path='directory/:campsiteId'
--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,10 +1,12 @@
 
 import { Container, Row, Col } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
+import SubHeader from '../components/SubHeader';
 
 const CampsitesDirectoryPage = () => {
     return (
         <Container>
+            <SubHeader current='Directory' />
             <CampsitesList />  
         </Container>
     );
